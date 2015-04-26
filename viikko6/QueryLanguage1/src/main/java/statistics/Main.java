1735872c9793a6e01687bcc731e23e4f7152bbe0
Katri Roos <katri.roos@cs.helsinki.fi>
2015-04-26 19:38:14 +0300

    5 tehtavaa tehty

--- viikko6/QueryLanguage1/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage1/src/main/java/statistics/Main.java
@@ -14,5 +14,37 @@ public class Main {
         for (Player player : stats.matches(m)) {
             System.out.println( player );
         }
+        
+        System.out.println("****************");
+        
+         Matcher m2 = new And( new HasFewerThan(10, "goals"),
+                             new HasFewerThan(10, "assists"),
+                             new PlaysIn("PHI")
+        );
+        
+        for (Player player : stats.matches(m2)) {
+            System.out.println( player );
+        }
+        
+        System.out.println("****************");
+        
+         Matcher m3 = new Not( new HasFewerThan(20, "goals"),
+                             new HasFewerThan(20, "assists"),
+                             new PlaysIn("PHI")
+        );
+        
+        for (Player player : stats.matches(m3)) {
+            System.out.println( player );
+        }
+        
+        System.out.println("****************");
+        
+         Matcher m4 = new Or( new HasAtLeast(30, "goals"),
+                             new HasAtLeast(30, "assists")
+        );
+        
+        for (Player player : stats.matches(m4)) {
+            System.out.println( player );
+        }
     }
 }
